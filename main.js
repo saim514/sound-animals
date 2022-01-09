3fefffe0841b468df83ee42c9a430804b14fe905
saim514 <81713694+saim514@users.noreply.github.com>
2022-01-09 13:33:14 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function startClassification()
+{
+    navigator.mediaDevices.getUserMedia({audio: true});
+    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/fPPKAC8XK/model.json" , modelReady)
+}
+
+function modelReady()
+{
+    classifier.classify(gotResults);
+}
+
+function gotResults(error,results)
+{
+    console.log('got result');
+}
