d6e12950b4f7d743f0b64f233e48f3094d9a15a3
saim514 <81713694+saim514@users.noreply.github.com>
2022-01-15 13:31:59 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -11,5 +11,32 @@ function modelReady()
 
 function gotResults(error,results)
 {
-    console.log('got result');
+    if(error)
+    {
+     console.error(error);
+    } else{
+        console.log(results);
+        random_number_r = Math.floor(Math.random() * 255) + 1;
+        random_number_g = Math.floor(Math.random() * 255) + 1;
+        random_number_b = Math.floor(Math.random() * 255) + 1;
+
+     document.getElementById("result_label").innerHTML = "I can hear - " + results[0].label;
+
+     document.getElementById("result_label").style.color = "rgb(" + random_number_r + "," + random_number_g + "," + random_number_b + ")";
+    
+     img = document.getElementById('ear.png');
+
+     if (results[0].label == "barking")
+     {
+         img.src="giphy-downsized-large.gif"
+     } else if(results[0].label == "meowing")
+     {
+        img.src="cat-cute.gif";
+     } else
+     {
+        img.src="pigeon-vibes.gif";
+     }
+    }
+    
 }
+
