d280b28eca9e5c6911148b126748d968d61a728c
saim514 <81713694+saim514@users.noreply.github.com>
2022-01-18 17:57:02 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -9,6 +9,10 @@ function modelReady()
     classifier.classify(gotResults);
 }
 
+var barking=0;
+var meowing=0;
+var chirping=0;
+
 function gotResults(error,results)
 {
     if(error)
@@ -20,8 +24,8 @@ function gotResults(error,results)
         random_number_g = Math.floor(Math.random() * 255) + 1;
         random_number_b = Math.floor(Math.random() * 255) + 1;
 
-     document.getElementById("result_label").innerHTML = "I can hear - " + results[0].label;
-
+     document.getElementById("result_label").innerHTML = "Detected Voice Is " + results[0].label;
+     document.getElementById("result_count").innerHTML = 'Detected Dog - '+ barking + 'Detected Cat - '+ meowing + 'Detected Bird - '+ chirping;
      document.getElementById("result_label").style.color = "rgb(" + random_number_r + "," + random_number_g + "," + random_number_b + ")";
     
      img = document.getElementById('ear.png');
@@ -29,9 +33,11 @@ function gotResults(error,results)
      if (results[0].label == "barking")
      {
          img.src="giphy-downsized-large.gif"
+         barking = barking + 1;
      } else if(results[0].label == "meowing")
      {
         img.src="cat-cute.gif";
+        meowing = meowing + 1;
      } else
      {
         img.src="pigeon-vibes.gif";
